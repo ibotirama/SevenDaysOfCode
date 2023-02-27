ca7393c03add16b3860a198941c47e662e60a786
ibotirama <pedro.henrique1@gmail.com>
2023-02-27 08:26:31 +0200

    Day 2

--- src/App.java
+++ src/App.java
@@ -3,6 +3,11 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -16,6 +21,47 @@ public class App {
             .build()
             .send(request, BodyHandlers.ofString());
 
-        System.out.println(send.body());
+        String json = send.body();
+
+        String[] moviesArray = parseJsonMovies(json);
+
+		List<String> titles = parseTitles(moviesArray);
+		titles.forEach(System.out::println);
+
+		List<String> urlImages = parseUrlImages(moviesArray);
+		urlImages.forEach(System.out::println);
+		
+		//outras listas para rating e years
     }
+
+    private static String[] parseJsonMovies(String json) {
+		Matcher matcher = Pattern.compile(".*\\[(.*)\\].*").matcher(json);
+
+		if (!matcher.matches()) {
+			throw new IllegalArgumentException("no match in " + json);
+		}
+
+		String[] moviesArray = matcher.group(1).split("\\},\\{");
+		moviesArray[0] = moviesArray[0].substring(1);
+		int last = moviesArray.length - 1;
+		String lastString = moviesArray[last];
+		moviesArray[last] = lastString.substring(0, lastString.length() - 1);
+		return moviesArray;
+	}
+	
+	private static List<String> parseTitles(String[] moviesArray) {
+		return parseAttribute(moviesArray, 3);
+	}
+	
+	private static List<String> parseUrlImages(String[] moviesArray) {
+		return parseAttribute(moviesArray, 5);
+	}
+	
+	private static List<String> parseAttribute(String[] moviesArray, int pos) {
+		return Stream.of(moviesArray)
+			.map(e -> e.split("\",\"")[pos]) 
+			.map(e -> e.split(":\"")[1]) 
+			.map(e -> e.replaceAll("\"", ""))
+			.collect(Collectors.toList());
+	}
 }
